ceae42bf217bcefeadf02b6e6b47170ed5b2a448
rogpe <prog_x@hotmail.com>
2020-08-24 16:16:05 -0300

    Updates

---
+++ precache-manifest.b69f5cf72c8ce61e42d5a35ef102b491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f266964a3dafb2b381c6cd321bc664b0",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a4c1f300ed838d422d61",
+    "url": "/react-deploy/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a4c1f300ed838d422d61",
+    "url": "/react-deploy/static/js/2.b56a970f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-deploy/static/js/2.b56a970f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0d78956a1a711b76c8",
+    "url": "/react-deploy/static/js/main.a7081390.chunk.js"
+  },
+  {
+    "revision": "3f052c08d62333b68ae8",
+    "url": "/react-deploy/static/js/runtime-main.b008f401.js"
+  },
+  {
+    "revision": "df1394f73a996c624e15085dc8a39687",
+    "url": "/react-deploy/static/media/leprechau.df1394f7.png"
+  }
+]);
